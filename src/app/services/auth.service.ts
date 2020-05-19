6a335ce6598f4933856d48a8c8b7c9176f179530
Antonio Montalva <tonimontalvapastor@gmail.com>
2020-05-19 17:01:28 +0200

    profile

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,15 +4,22 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { ToastController, LoadingController } from '@ionic/angular';
 import * as firebase from 'firebase';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class AuthService {
 
     error: string = "";
+    lang: string = "es";
 
-    constructor(public afAuth: AngularFireAuth, private router: Router, private toastController: ToastController, private loadingController: LoadingController) {
-
-    }
+    constructor(
+        public afAuth: AngularFireAuth,
+        private router: Router, 
+        private toastController: ToastController, 
+        private loadingController: LoadingController, 
+        private translate: TranslateService
+        ) {            
+        }
 
     // shows a message to the user in top, middle or bottom position
     async presentToast(message, position, duration) {
@@ -26,26 +33,40 @@ export class AuthService {
 
     // shows a message while the app is running the backend
     async presentLoading() {
-        const loading = await this.loadingController.create({
-            message: 'Please wait...',
-            duration: 1200 // 1.2s
-        });
-        await loading.present();
-
-        const { role, data } = await loading.onDidDismiss();
-        console.log('Loading dismissed!');
+        this.lang = this.translate.currentLang;
+        if(this.lang == "en") {
+            const loading = await this.loadingController.create({
+                message: 'Please wait...',
+                duration: 1200 // 1.2s
+            });
+            await loading.present();
+            const { role, data } = await loading.onDidDismiss();
+        } else if(this.lang == "es") {
+            const loading = await this.loadingController.create({
+                message: 'Por favor espera...',
+                duration: 1200 // 1.2s
+            });
+            await loading.present();
+            const { role, data } = await loading.onDidDismiss();
+            console.log('Loading dismissed!');
+        }
     }
 
     // Login with email and password
     async login(email: string, password: string) {
         this.error = "";
+        this.lang = this.translate.currentLang;
         this.presentLoading();
         await this.afAuth.auth.signInWithEmailAndPassword(email, password)
         .then(data => {
             if (data.user) {
                 // user logged, we welcome him 
                 this.router.navigate(['/tabs/tab1']);
-                this.presentToast('Welcome ' + email + '!', 'bottom', 2000);
+                if(this.lang == "es") {
+                    this.presentToast('¡Bienvenido ' + email + '!', 'bottom', 2000);
+                } else if(this.lang == "en") {
+                    this.presentToast('Welcome ' + email + '!', 'bottom', 2000);
+                }                
             }
         })
         .catch(error => {
