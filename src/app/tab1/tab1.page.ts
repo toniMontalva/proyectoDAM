e9ecc0a792842ac476ea13271ab989a070ac7278
Antonio Montalva <tonimontalvapastor@gmail.com>
2020-05-25 15:26:51 +0200

    future matches api

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { MatchesService } from '../services/data/matches.service';
 import { Observable } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-tab1',
@@ -18,7 +19,13 @@ export class Tab1Page implements OnInit {
   constructor(private _authService: AuthService, private router: Router, private _dataService: MatchesService) {}
 
   async ngOnInit() {
-    this.data = await this._dataService.getData('matches')['matches'];
+    //this.data = await this._dataService.getData('matches')['matches'];
+    let filter = 'dateFrom=';
+    filter += this.currentDate();
+    filter += '&dateTo=';
+    filter += this.currentDatePlusWeek();
+    console.log(filter);
+    this.dataSoon = await this._dataService.getDataFilter('matches', filter)['matches'];
   }
 
   async logout() {
@@ -33,6 +40,14 @@ export class Tab1Page implements OnInit {
     console.log(this.segmentModel);
     
     console.log(event);
+  }
+
+  currentDate() : string {
+    return moment().format('YYYY-MM-DD');
+  }
+
+  currentDatePlusWeek() : string {
+    return moment().add(10, 'days').format('YYYY-MM-DD');
   }
 
 }
