15643ee4363ad457ae3fffc00c289a5be24d6c74
Antonio Montalva <tonimontalvapastor@gmail.com>
2020-05-27 18:27:29 +0200

    minimal updates

--- src/app/match-details/match-details.page.ts
+++ src/app/match-details/match-details.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MatchesService } from '../services/data/matches.service';
 import * as moment from 'moment';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-match-details',
@@ -12,22 +13,29 @@ export class MatchDetailsPage implements OnInit {
 
   id: number;
   league: string = "";
+  // home team variables
   homeTeam: string = "";
   homeTeamPic: string = "";
   homeTeamId: number;
+  // away team variables
   awayTeam: string = "";
   awayTeamPic: string = "";
   awayTeamId: number;
+  // match variables
   matchTime: string = "";
-  head2head: any[] = [];
+  //head2head: any[] = [];
   venue: string = "";
+  // handling variables
   value: string = "";
+  error: string = "";
 
 
-  constructor(private _activatedRoute: ActivatedRoute, private _dataService: MatchesService) { }
+  constructor(private _activatedRoute: ActivatedRoute, private _dataService: MatchesService, private toastCrtl: ToastController) { }
 
   ngOnInit() {
     this.id = +this._activatedRoute.snapshot.paramMap.get('id');
+    this.error = "";
+    // request the match info to the API
     this._dataService.getMatchInfo(this.id)
       .subscribe(
         (data) => { // Success
@@ -45,14 +53,21 @@ export class MatchDetailsPage implements OnInit {
         }
       );
 
+    // i set a timeout to get the teams info because of async purpose with last request.
     setTimeout(() => {
       this.getTeamHomeInfo();
     }, 2000);
     setTimeout(() => {
       this.getTeamAwayInfo();
     }, 2000);
+
+    // if there's any error with API, request it again text shows up
+    if(!this.homeTeamPic || !this.awayTeamPic) {
+      this.error = "API error, pull to refresh.";
+    }
   }
 
+  // obtain the info of the home team(picture and code name)
   getTeamHomeInfo() {
     this._dataService.getTeamInfo(this.homeTeamId)
       .subscribe(data => {
@@ -60,8 +75,12 @@ export class MatchDetailsPage implements OnInit {
         this.homeTeamPic = data['crestUrl'];
         this.homeTeam = data['tla'];
       });
+    if(!this.homeTeamPic) {
+      this.error = "API error, pull to refresh.";
+    }
   }
 
+  // obtain the info of the away team(picture and code name)
   getTeamAwayInfo() {
     this._dataService.getTeamInfo(this.awayTeamId)
       .subscribe(data => {
@@ -69,18 +88,37 @@ export class MatchDetailsPage implements OnInit {
         this.awayTeamPic = data['crestUrl'];
         this.awayTeam = data['tla'];
       });
+    if(!this.awayTeamPic) {
+      this.error = "API error, pull to refresh.";
+    }
   }
 
   doRefresh(event) {
+    this.error = "";
+    // sometimes API doesn't give us both info at once so we can request the one's info left
     if(!this.homeTeamPic) {
       this.getTeamHomeInfo();      
     } else if(!this.awayTeamPic) {
       this.getTeamAwayInfo();
     }
 
+    // main error
+    if(this.league == "") {
+      this.ngOnInit();
+    }
+
     setTimeout(() => {
       event.target.complete();
     }, 2000);
+  }
+
+  async presentToast() {
+    const toast = await this.toastCrtl.create({
+      message: 'Ahora serás(supuestamente) redirigido a Bet365.',
+      color: 'dark',
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
